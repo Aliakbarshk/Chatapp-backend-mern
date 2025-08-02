88647a37e050f58177dac2199399f065fbabeb7b
Aliakbarshk <aliakbarsk05@gmail.com>
2025-08-02 09:27:27 +0530

    completed login and logout functionality

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {signup,login,logout} from "../controllers/auth.controller.js";
+import {signup,login,logout, updateProfile} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -7,6 +7,8 @@ const router = express.Router();
 router.post("/signup", signup);// at here there is just callbacks and routes that have been seted up
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.put("/update-profile",protectRoute,updateProfile)
 
 
 
